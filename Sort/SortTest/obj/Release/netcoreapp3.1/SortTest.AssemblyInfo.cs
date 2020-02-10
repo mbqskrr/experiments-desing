2d5c2ceb0bfc9dd8588655faf839bdab00d6c105
Manuel Balanta Quintero <1005943823@academico.icesi.edu.co>
2020-02-10 16:29:33 -0500

    Eliminado archivo basura

--- Sort/SortTest/obj/Release/netcoreapp3.1/SortTest.AssemblyInfo.cs
+++ Sort/SortTest/obj/Release/netcoreapp3.1/SortTest.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SortingTest")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SortTest")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("SortingTest")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SortingTest")]
+[assembly: System.Reflection.AssemblyProductAttribute("SortTest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SortTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
